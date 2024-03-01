0f8cfa33cdb94b80c055b9e42d7611b37db6d63e
Rich Morgan <ionux@users.noreply.github.com>
2024-02-29 22:59:34 -0700

    Formatting & style consistency.

--- api/Print.cpp
+++ api/Print.cpp
@@ -31,10 +31,19 @@ using namespace arduino;
 size_t Print::write(const uint8_t *buffer, size_t size)
 {
   size_t n = 0;
-  while (size--) {
-    if (write(*buffer++)) n++;
-    else break;
+
+  while (size--)
+  {
+    if (write(*buffer++))
+    {
+      n++;
+    }
+    else
+    {
+      break;
+    }
   }
+
   return n;
 }
 
@@ -43,12 +52,26 @@ size_t Print::print(const __FlashStringHelper *ifsh)
 #if defined(__AVR__)
   PGM_P p = reinterpret_cast<PGM_P>(ifsh);
   size_t n = 0;
-  while (1) {
+
+  while (1)
+  {
     unsigned char c = pgm_read_byte(p++);
-    if (c == 0) break;
-    if (write(c)) n++;
-    else break;
+    
+    if (c == 0)
+    {
+      break;
+    }
+    
+    if (write(c))
+    {
+      n++;
+    }
+    else
+    {
+      break;
+    }
   }
+
   return n;
 #else
   return print(reinterpret_cast<const char *>(ifsh));
@@ -87,46 +110,67 @@ size_t Print::print(unsigned int n, int base)
 
 size_t Print::print(long n, int base)
 {
-  if (base == 0) {
+  if (base == 0)
+  {
     return write(n);
-  } else if (base == 10) {
-    if (n < 0) {
+  }
+  
+  if (base == 10)
+  {
+    if (n < 0)
+    {
       int t = print('-');
       n = -n;
+
       return printNumber(n, 10) + t;
     }
+
     return printNumber(n, 10);
-  } else {
-    return printNumber(n, base);
   }
+
+  return printNumber(n, base);
 }
 
 size_t Print::print(unsigned long n, int base)
 {
-  if (base == 0) return write(n);
-  else return printNumber(n, base);
+  if (base == 0)
+  {
+    return write(n);
+  }
+
+  return printNumber(n, base);
 }
 
 size_t Print::print(long long n, int base)
 {
-  if (base == 0) {
+  if (base == 0)
+  {
     return write(n);
-  } else if (base == 10) {
-    if (n < 0) {
+  }
+
+  if (base == 10)
+  {
+    if (n < 0)
+    {
       int t = print('-');
       n = -n;
       return printULLNumber(n, 10) + t;
     }
+  
     return printULLNumber(n, 10);
-  } else {
-    return printULLNumber(n, base);
   }
+
+  return printULLNumber(n, base);
 }
 
 size_t Print::print(unsigned long long n, int base)
 {
-  if (base == 0) return write(n);
-  else return printULLNumber(n, base);
+  if (base == 0)
+  {
+    return write(n);
+  }
+
+  return printULLNumber(n, base);
 }
 
 size_t Print::print(double n, int digits)
@@ -239,15 +283,20 @@ size_t Print::println(const Printable& x)
 
 size_t Print::printNumber(unsigned long n, uint8_t base)
 {
-  char buf[8 * sizeof(long) + 1]; // Assumes 8-bit chars plus zero byte.
+  // Assumes 8-bit chars plus zero byte.
+  char buf[8 * sizeof(long) + 1] = {0};
   char *str = &buf[sizeof(buf) - 1];
 
   *str = '\0';
 
   // prevent crash if called with base == 1
-  if (base < 2) base = 10;
-
-  do {
+  if (base < 2)
+  {
+    base = 10;
+  }
+  
+  do
+  {
     char c = n % base;
     n /= base;
 
@@ -283,22 +332,27 @@ size_t Print::printNumber(unsigned long n, uint8_t base)
 size_t Print::printULLNumber(unsigned long long n64, uint8_t base)
 {
   // if limited to base 10 and 16 the bufsize can be 20
-  char buf[64];
+  char buf[64] = {0};
   uint8_t i = 0;
   uint8_t innerLoops = 0;
 
   // Special case workaround https://github.com/arduino/ArduinoCore-API/issues/178
-  if (n64 == 0) {
+  if (n64 == 0)
+  {
     write('0');
     return 1;
   }
 
   // prevent crash if called with base == 1
-  if (base < 2) base = 10;
-
+  if (base < 2)
+  {
+    base = 10;
+  }
+  
   // process chunks that fit in "16 bit math".
   uint16_t top = 0xFFFF / base;
   uint16_t th16 = 1;
+  
   while (th16 < top)
   {
     th16 *= base;
@@ -322,6 +376,7 @@ size_t Print::printULLNumber(unsigned long long n64, uint8_t base)
   }
 
   uint16_t n16 = n64;
+
   while (n16 > 0)
   {
     uint16_t qq = n16/base;
@@ -330,19 +385,24 @@ size_t Print::printULLNumber(unsigned long long n64, uint8_t base)
   }
 
   size_t bytes = i;
+  
   for (; i > 0; i--)
-    write((char) (buf[i - 1] < 10 ?
-    '0' + buf[i - 1] :
-    'A' + buf[i - 1] - 10));
-
+  {
+    write(((char) (buf[i - 1] < 10)
+      ? '0' + buf[i - 1]
+      : 'A' + buf[i - 1] - 10));
+  }
+  
   return bytes;
 }
 
 size_t Print::printFloat(double number, int digits)
 {
   if (digits < 0)
+  {
     digits = 2;
-
+  }
+  
   size_t n = 0;
 
   if (isnan(number)) return print("nan");
@@ -359,8 +419,11 @@ size_t Print::printFloat(double number, int digits)
 
   // Round correctly so that print(1.999, 2) prints as "2.00"
   double rounding = 0.5;
-  for (uint8_t i=0; i<digits; ++i)
+
+  for (uint8_t i = 0; i < digits; ++i)
+  {
     rounding /= 10.0;
+  }
 
   number += rounding;
 
@@ -370,7 +433,8 @@ size_t Print::printFloat(double number, int digits)
   n += print(int_part);
 
   // Print the decimal point, but only if there are digits beyond
-  if (digits > 0) {
+  if (digits > 0)
+  {
     n += print(".");
   }
 
