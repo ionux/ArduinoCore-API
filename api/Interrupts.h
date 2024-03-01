3422e64488f2873b965b1c790b71a69905ffd6c8
Rich Morgan <ionux@users.noreply.github.com>
2024-02-29 22:45:20 -0700

    Formatting & style consistency updates.

--- api/Interrupts.h
+++ api/Interrupts.h
@@ -1,25 +1,29 @@
 #ifndef W_INTERRUPTS_CPP
 #define W_INTERRUPTS_CPP
+
 #ifdef __cplusplus
 
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
+
 #include "Common.h"
 
-namespace arduino {
+namespace arduino
+{
 
 template <typename T>
 using voidTemplateFuncPtrParam =  void (*)(T param);
 
-template<typename T> struct __container__ {
+template<typename T> struct __container__
+{
   void* param;
   voidTemplateFuncPtrParam<T> function;
 };
 
 // C++ only overloaded version of attachInterrupt function
-template<typename T> void attachInterrupt(pin_size_t interruptNum, voidTemplateFuncPtrParam<T> userFunc, PinStatus mode, T& param) {
-
+template<typename T> void attachInterrupt(pin_size_t interruptNum, voidTemplateFuncPtrParam<T> userFunc, PinStatus mode, T& param)
+{
   struct __container__<T> *cont = new __container__<T>();
   cont->param = &param;
   cont->function = userFunc;
@@ -35,10 +39,12 @@ template<typename T> void attachInterrupt(pin_size_t interruptNum, voidTemplateF
   attachInterruptParam(interruptNum, f, mode, cont);
 }
 
-template<typename T> void attachInterrupt(pin_size_t interruptNum, voidTemplateFuncPtrParam<T*> userFunc, PinStatus mode, T* param) {
+template<typename T> void attachInterrupt(pin_size_t interruptNum, voidTemplateFuncPtrParam<T*> userFunc, PinStatus mode, T* param)
+{
   attachInterruptParam(interruptNum, (voidFuncPtrParam)userFunc, mode, (void*)param);
 }
 
 }
-#endif
-#endif
+
+#endif // __cplusplus
+#endif // W_INTERRUPTS_CPP
